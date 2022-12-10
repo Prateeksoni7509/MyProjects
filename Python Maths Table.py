7fea5550948df869f5e81f66c9eb5d1b354b1ef8
PRATEEK SONI <117587880+Prateeksoni7509@users.noreply.github.com>
2022-12-10 16:13:02 +0530

    Update Python Maths Table.py

--- Python Maths Table.py
+++ Python Maths Table.py
@@ -5,6 +5,7 @@ class table:
     def printTable(self):
         for i in range(10):
             print(self.num,' x ',i+1,' = ',self.num*(i+1))
+print('This is a multiplication table application.')
 n=int(input('Enter the number : '))
 for x in range(1,n+1):
     a=table(x)
