f010eec5f2cda2cf8d28fd42fddc46142643d19a
harshm413 <119815007+harshm413@users.noreply.github.com>
2022-12-10 15:54:00 +0530

    Add files via upload

---
+++ Python Maths Table.py
@@ -0,0 +1,13 @@
+#Multiplication Table Application
+class table:
+    def __init__(self,num):
+        self.num=num
+    def printTable(self):
+        for i in range(10):
+            print(self.num,' x ',i+1,' = ',self.num*(i+1))
+n=int(input('Enter the number : '))
+for x in range(1,n+1):
+    a=table(x)
+    print('------------------')
+    print('Table of',x,': ')
+    a.printTable()
